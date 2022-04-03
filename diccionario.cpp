0095438cb623d5aeabb94f4a087fab1ca8c0ef72
Saikora11 <pedro.garciaromera@alum.uca.es>
2022-04-03 19:34:48 +0200

    jbjlkkljhil

--- diccionario.cpp
+++ diccionario.cpp
@@ -1,13 +1,47 @@
 #include <iostream>
 #include <map>
+#include <fstream>
 
 using namespace std;
 
 class Diccionario{
     public:
         explicit Diccionario(string nom = "");
+        inline size_t entradas() const { return dic.size();}
+        inline void introduce(string pal1, string pal2) { dic[pal1] = pal2;}
+        inline string traduccion(string pal1){ return dic[pal1];} 
+        pair<string,string> operator [](size_t);
     private:
         map<string,string> dic;
 };
 
-Diccionario::Diccionario(s)
+Diccionario::Diccionario(string nom){
+    fstream fich(nom);
+    string cad;
+    string pal1, pal2;
+    
+    while(!getline(fich, cad)){
+        pal1 = cad.substr(0,cad.find(" "));
+        pal2 =  cad.substr(cad.find(" ")+1, cad.size());
+
+        dic[pal1] = pal2;
+    }
+}
+
+pair<string,string> Diccionario::operator [](size_t n){
+    map<string,string>::iterator it = dic.begin();
+
+    size_t ind = 0;
+
+    while(it != dic.end()){
+        if (ind == n) return pair<string,string>(it->first, it->second);
+        it++;
+        ind++;
+    }
+
+    return pair<string,string>("", "");
+}
+
+
+
+
