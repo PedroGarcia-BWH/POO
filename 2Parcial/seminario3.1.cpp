b4a97f30297c7885332d44ad63724d6293b50689
Saikora11 <pedro.garciaromera@alum.uca.es>
2022-05-09 21:38:15 +0200

    2pa

---
+++ 2Parcial/seminario3.1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <set>
+#include <map>
+using namespace std;
+// namespace std;
+
+
+class Profesor{
+    public:
+
+    private:
+        set<Grupo*> Imp;
+}
+
+
+class Grupo{
+    public:
+
+    private:
+        set<Grupo*> Imp;
+}
